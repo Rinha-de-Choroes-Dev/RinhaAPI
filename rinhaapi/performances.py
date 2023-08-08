76a8c19a91c0e6600003e2e6512b668886523c8b
Victor Raposo <victor.ravaglia@hotmail.com>
2023-08-08 18:03:12 -0300

    Added more tests and now card stats are computed (without being normalized))

--- rinhaapi/performances.py
+++ rinhaapi/performances.py
@@ -2,7 +2,7 @@ import psycopg2
 from .config import *
 import numpy as np
 import os.path
-from enum import Enum
+from enum import IntEnum
 import pandas as pd
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -40,6 +40,29 @@ db_headers_team = np.array([
                 ['img', str],
                 ['team', int]])
 
+def validate_pint(n, max = 1000):
+    if not isinstance(n, int):
+        return -1
+    if n < 0 or n > max:
+        return -1
+    return n
+
+class Tables(IntEnum):
+    MATCHES_RINHA3 = 0
+    PLAYERS_RINHA3 = 1
+    MOCK_PLAYERS = 2
+
+table_strings = ['matches_rinha3',
+               'players_rinha3',
+               'mock_players']
+
+def validate_table(table_idx):
+    if not isinstance(table_idx, Tables):
+        valid_pint = validate_pint(table_idx, len(table_strings) -1)
+        if valid_pint == -1:
+            return -1
+
+    return table_strings[table_idx]
 
 class PlayerMetrics():
 
@@ -65,19 +88,70 @@ class PlayerMetrics():
         return lanes.sum()/len(lanes)
     
     def compute_fantasy_points(performances):
-
         
-        
-        weighted = performances.copy()[stat_list[:, 0]]
-        for stat, weight in stat_list:
+        weighted = performances.copy()[PlayerMetrics.stat_list[:, 0]]
+        for stat, weight in PlayerMetrics.stat_list:
             weighted[stat] = weighted[stat].astype('float')*float(weight)
 
-        weighted['sum'] = weighted.sum(axis='columns')
+        # weighted['sum'] = weighted.sum(axis='columns')
 
-        return weighted
+        return weighted.sum(axis='columns')
+
+    def compute_versat(performances):
+        return len(performances.hero_id.unique())/len(performances.hero_id)
+    
+    def compute_kda(performances):
+        return (performances.kills.astype('int32')+performances.assists.astype('int32'))/(performances.deaths.astype('int32') + 1)
+    
+    def compute_farm(performances):
+        w1 = 1
+        w2 = 0.8
+        return performances.gpm.astype('int32')*w1 + performances.lh.astype('int32')*w2
+    
+    def compute_tf(performances):
+        w1 = 1.2
+        w2 = 1
+        w3 = 0.5
+        return (performances.stun.astype('int32')*w1 + performances.healing.astype('int32')*w2 + performances.damage_hero.astype('int32')*w3)/performances.duration.astype('int32')
+    
+    def compute_supp(performances):
+        w1 = 1000
+        w2 = 700
+        w3 = 3000
+        return (performances.wards.astype('int32')*w1 + performances.sentries.astype('int32')*w2 + performances.stacks.astype('int32')*w3)/performances.duration.astype('int32')
+    
+    def compute_imp(performances):
+        return performances.imp.astype('float')
+    
+    def compute_card_stats(performances):
+        return {"fps": PlayerMetrics.compute_fantasy_points(performances), 
+                "imp": PlayerMetrics.compute_imp(performances), 
+                "lwr": PlayerMetrics.compute_lane_stat(performances), 
+                "vst": PlayerMetrics.compute_versat(performances), 
+                "kda": PlayerMetrics.compute_kda(performances), 
+                "frm": PlayerMetrics.compute_farm(performances),
+                "tfi": PlayerMetrics.compute_tf(performances),
+                "sup": PlayerMetrics.compute_supp(performances)}
+    
+    def compute_card_stats_avg(performances):
+        stats = PlayerMetrics.compute_card_stats(performances)
+        avg_stats = {}
         
+        for k, v in stats.items():
+            avg_stats[k] = np.mean(v)
 
-def get_player_performances(player, table_name):
+        return avg_stats
+
+def get_player_performances(player, table_idx):
+
+    player = validate_pint(player, 8)
+    if player == -1:
+        return
+    
+    table_name = validate_table(table_idx)
+    if table_name == -1:
+        return
+
     conn = psycopg2.connect(database=db_name,
                     host=db_host,
                     user=db_user,
@@ -99,7 +173,19 @@ def get_player_performances(player, table_name):
 
     return df
 
-def get_team_performances(team, table_name):
+def get_team_performances(team, team_table_idx, player_table_idx):
+    team = validate_pint(team)
+    if team == -1:
+        return
+    
+    team_table = validate_table(team_table_idx)
+    if team_table == -1:
+        return
+    
+    player_table = validate_table(player_table_idx)
+    if player_table == -1:
+        return
+
     conn = psycopg2.connect(database=db_name,
                     host=db_host,
                     user=db_user,
@@ -108,12 +194,13 @@ def get_team_performances(team, table_name):
     
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM players WHERE team=" + str(team))
+    query = "SELECT * FROM " + team_table + " WHERE team=" + str(team)
+    cursor.execute(query)
 
     players = np.array(cursor.fetchall())
     df_players = pd.DataFrame(players, columns = db_headers_team[:, 0])
 
-    query = "SELECT * FROM " + table_name
+    query = "SELECT * FROM " + player_table
     first = True
 
     for player_id in df_players.steamid:
@@ -131,24 +218,53 @@ def get_team_performances(team, table_name):
     for player_id in df_players.steamid:
         all_performances.append(df_stats.loc[df_stats['steamid'] == player_id])
 
-    return all_performances
+    return df_players, all_performances
+
+def get_all_performances(team_table_idx, player_table_idx):
+    team_table = validate_table(team_table_idx)
+    if team_table == -1:
+        return
+    
+    player_table = validate_table(player_table_idx)
+    if player_table == -1:
+        return
+
+    conn = psycopg2.connect(database=db_name,
+                    host=db_host,
+                    user=db_user,
+                    password=db_pass,
+                    port=db_port)
+    
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM " + team_table
+    cursor.execute(query)
+
+    players = np.array(cursor.fetchall())
+    df_players = pd.DataFrame(players, columns = db_headers_team[:, 0])
+
+    query = "SELECT * FROM " + player_table
+
+    cursor.execute(query)
+    stats = np.array(cursor.fetchall())
+    df_stats = pd.DataFrame(stats, columns = db_headers_player[:, 0])
+
+    all_performances = []
+    for player_id in df_players.steamid:
+        all_performances.append(df_stats.loc[df_stats['steamid'] == player_id])
+
+    return df_players, all_performances
+
+def get_all_cards(team_table_idx, player_table_idx):
+    df_players, all_performances = get_all_performances(team_table_idx, player_table_idx)
+    for i in range(len(all_performances)):
+        perf = all_performances[i]
+        stats = PlayerMetrics.compute_card_stats_avg(perf)
+        print(pd.DataFrame.from_dict(stats))
+
 
 def debug():
 
-    # ids = [ ['100274651','Rafael'],
-    #         ['110919290','Derig'],
-    #         ['98456065','Raposo'],
-    #         ['120894677','Bubigam'],
-    #         ['106633036','Lemilton']]
-    
-    # for id, name in ids:
-    #     perf = get_player_performances(id)
-    #     print("\n" + name)
-    #     print(PlayerMetrics.compute_lane_stat(perf))
-    #     print(PlayerMetrics.compute_fantasy_points(perf))
+    all_cards = get_all_cards(Tables.PLAYERS_RINHA3, Tables.MATCHES_RINHA3)
 
-    all_performances = get_team_performances(0, "matches_rinha3")
-
-    for perf in all_performances:
-        print(PlayerMetrics.compute_lane_stat(perf))
-        print(PlayerMetrics.compute_fantasy_points(perf))
+    return
--- tests/test_performances.py
+++ tests/test_performances.py
@@ -18,5 +18,61 @@ def test_compute_lane_stat():
     assert generate_mock_lane_stat([10,-5,-10,5]) == 0
     assert generate_mock_lane_stat([10,5,5,10]) == 30/4
 
+def test_validate_pint():
+    assert validate_pint(5) == 5
+    assert validate_pint(0) == 0
+    assert validate_pint(10, 8) == -1
+    assert validate_pint(-5) == -1
+    assert validate_pint(3.14) == -1
+    assert validate_pint(3.0) == -1
+    assert validate_pint('team') == -1
+
+def test_validate_table():
+    assert validate_table(0) == 'matches_rinha3'
+    assert validate_table(Tables.PLAYERS_RINHA3) == 'players_rinha3'
+    assert validate_table(-1) == -1
+    assert validate_table(4) == -1
+    assert validate_table(None) == -1
+
+def test_compute_lane_stat():
+    performance = pd.DataFrame({'won_lane': [1]})
+    assert PlayerMetrics.compute_lane_stat(performance) == 1
+    performance = pd.DataFrame({'won_lane': [1, 2, 0]})
+    assert PlayerMetrics.compute_lane_stat(performance) == 1
+    performance = pd.DataFrame({'won_lane': [-1, -1]})
+    assert PlayerMetrics.compute_lane_stat(performance) == -1
+    performance = pd.DataFrame({'won_lane': [-2, -1]})
+    assert PlayerMetrics.compute_lane_stat(performance) == -3/2
+
 def test_compute_fantasy_points():
-    assert PlayerMetrics.stat_list == 0
+    performances = pd.DataFrame({
+            'kills': [10, 5, 3],
+            'deaths': [2, 1, 0],
+            'assists': [5, 3, 2],
+            'lh': [200, 150, 100],
+            'gpm': [500, 400, 300],
+            'xpm': [600, 500, 400],
+            'stun': [10, 5, 2],
+            'healing': [1000, 500, 200],
+            'damage_tower': [5000, 3000, 1000],
+            'stacks': [5, 3, 2],
+            'wards': [10, 5, 2],
+            'sentries': [5, 3, 1]
+        })
+    expected_output = pd.DataFrame({
+        'kills': [3.0, 1.5, 0.9],
+        'deaths': [-0.6, -0.3, -0.0],
+        'assists': [0.75, 0.45, 0.3],
+        'lh': [0.6, 0.45, 0.3],
+        'gpm': [2.0, 1.6, 1.2],
+        'xpm': [2.4, 2.0, 1.6],
+        'stun': [0.004, 0.002, 0.0008],
+        'healing': [0.3, 0.15, 0.06],
+        'damage_tower': [1.5, 0.9, 0.3],
+        'stacks': [2.5, 1.5, 1.0],
+        'wards': [3.0, 1.5, 0.6],
+        'sentries': [1.5, 0.9, 0.3],
+        'sum': [16.954, 10.652, 6.5608]
+    })
+    output = PlayerMetrics.compute_fantasy_points(performances)
+    pd.testing.assert_frame_equal(output, expected_output)
